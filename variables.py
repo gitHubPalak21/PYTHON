b472de20bbabc2ef178ea2329d713336ac82d4a9
gitHubPalak21 <148367606+gitHubPalak21@users.noreply.github.com>
2025-02-09 00:22:48 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,28 @@
+a=1 # a is an integer
+b=2 # b is a floating point number
+print(a+b)
+d= False #d is boolean
+e = None #e is none type varible
+#comparison operators
+d= 4>6
+print(d)
+print(not(True))
+#type identification
+#t=type(a)
+#type conversion
+a="56.9"
+b=float(a)
+print(b)
+a= input("enter the number 1") #write now it inputs in form of string
+b= input("enter the number 2")
+print("first no is \n",a)
+print("first no is \n",b)
+print("sum of the no is \n",a+b)
+
+a= int(input("enter the number 1")) #write now it inputs in form of int
+b= int(input("enter the number 2"))
+print("first no is \n",a)
+print("first no is \n",b)
+print("sum of the no is\n",a+b)
+
+
